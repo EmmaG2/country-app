aa8322a30a50d9aff3eccb322d6c1416002a53cb
ItsMeEmmanuelG2 <itsmeemmadev@gmail.com>
2021-11-09 15:00:00 -0600

    por-continente component working

--- src/app/pais/pages/por-continente/por-continente.component.ts
+++ src/app/pais/pages/por-continente/por-continente.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
+import { Pais } from '../../inferfaces/pais.interface';
 
 @Component({
   selector: 'app-por-continente',
@@ -6,11 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class PorContinenteComponent implements OnInit {
+export class PorContinenteComponent {
 
-  constructor() { }
+  continentes:string[] = ['africa', 'americas', 'asia', 'europe', 'oceania' ];
+  continenteActivo:string = '';
 
-  ngOnInit(): void {
-  }
+  termino:string = '';
+  hayError: boolean = false;
+  paises: Pais[] = [];
 
-}
+  constructor( private paisService:PaisService ) { }
+
+  activarContinente( continente:string ) {
+    this.continenteActivo = continente;
+
+    this.hayError = false;
+    this.termino = continente;
+
+    this.paisService.buscarContinente(this.termino).subscribe(
+        (continente) => {
+            this.paises = continente;
+        },
+        (err) => {
+            this.hayError = true;
+            this.paises = [];
+        }
+    );
+  };
+
+  getClassCss ( continente:string ):string {
+    return continente === this.continenteActivo
+        ? 'btn btn-primary btn-sm m-1'
+        : 'btn btn-outline-primary btn-sm m-1';
+  };   
+};
