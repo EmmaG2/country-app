cca7e49948b02633ff7cc8cb5dd8f4161de40e64
ItsMeEmmanuelG2 <itsmeemmadev@gmail.com>
2021-11-09 15:00:22 -0600

    buscarContinente method added

--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -4,25 +4,30 @@ import { Observable } from 'rxjs';
 import { Pais } from '../inferfaces/pais.interface';
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class PaisService {
-  private apiUrl: string = 'https://restcountries.com/v3.1';
+    private apiUrl: string = 'https://restcountries.com/v3.1';
 
-  constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient) {}
 
-  buscarPais(termino: string): Observable<Pais[]> {
-    const url = `${this.apiUrl}/name/${termino}?fullText=true'`;
-    return this.http.get<Pais[]>(url);
-  };
+    buscarPais(termino: string): Observable<Pais[]> {
+        const url = `${this.apiUrl}/name/${termino}?fullText=true'`;
+        return this.http.get<Pais[]>(url);
+    };
 
-  buscarCapital(termino: string): Observable<Pais[]> {
-    const url = `${this.apiUrl}/capital/${termino}`;
-    return this.http.get<Pais[]>(url);
-  };
+    buscarCapital(termino: string): Observable<Pais[]> {
+        const url = `${this.apiUrl}/capital/${termino}`;
+        return this.http.get<Pais[]>(url);
+    };
 
-  getIdCountry (termino: string): Observable<Pais> {
-    const url = `${this.apiUrl}/alpha/${termino}`;
-    return this.http.get<Pais>(url);
-  };
+    getIdCountry(termino: string): Observable<Pais> {
+        const url = `${this.apiUrl}/alpha/${termino}`;
+        return this.http.get<Pais>(url);
+    };
+
+    buscarContinente(termino: string): Observable<Pais[]> {
+        const url = `${this.apiUrl}/region/${termino}`;
+        return this.http.get<Pais[]>(url);
+    };
 };
