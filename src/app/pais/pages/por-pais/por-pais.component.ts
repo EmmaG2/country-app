6954ad4a672d9d5cefa7474b0d9544c5341d45a8
ItsMeEmmanuelG2 <itsmeemmadev@gmail.com>
2021-11-10 13:10:58 -0600

    suggestions  added

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -3,31 +3,53 @@ import { PaisService } from '../../services/pais.service';
 import { Pais } from '../../inferfaces/pais.interface';
 
 @Component({
-  selector: 'app-por-pais',
-  templateUrl: './por-pais.component.html'
+    selector: 'app-por-pais',
+    templateUrl: './por-pais.component.html',
+    styles: [
+        `
+            li {
+                cursor: pointer;
+            }
+        `,
+    ],
 })
-
 export class PorPaisComponent {
+    termino: string = '';
+    hayError: boolean = false;
+    paises: Pais[] = [];
 
-  termino:string = '';
-  hayError: boolean = false;
-  paises: Pais[] = [];
+    paisesSugeridos: Pais[] = [];
+    mostrarSugerencias: boolean = false;
 
-  constructor( private paisService:PaisService ) { }
+    constructor(private paisService: PaisService) {}
 
-  buscar( termino:string ) {
+    buscar(termino: string) {
+        this.hayError = false;
+        this.termino = termino;
 
-    this.hayError = false;
-    this.termino = termino;
-    
-    this.paisService.buscarPais( this.termino )
-    .subscribe( (pais) => {
-      this.paises = pais;
-    }, 
-    (err) => {
-      this.hayError = true;
-      this.paises = [];
-    });
-  };
+        this.paisService.buscarPais(this.termino).subscribe(
+            (pais) => {
+                this.paises = pais;
+            },
+            (err) => {
+                this.hayError = true;
+                this.paises = [];
+            }
+        );
+    }
 
+    sugerencias(pais: string) {
+        this.hayError = false;
+        this.termino = pais;
+        this.mostrarSugerencias = true;
+
+        this.paisService.buscarPais(pais).subscribe(
+            (paises) => (this.paisesSugeridos = paises.splice(0, 6)),
+            (err) => (this.paisesSugeridos = [])
+        );
+    }
+
+    buscarSugerido(termino: string) {
+        this.buscar(termino);
+    }
 };
